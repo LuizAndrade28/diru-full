00bce7407d2677fc1a761d22d28970cf0371edf3
LuizAndrade <lcfsandrade@gmail.com>
2025-05-30 16:39:42 -0300

    Adjusted family creation and invite

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,13 +3,13 @@ class InvitesController < ApplicationController
 
   # POST /invites
   def create
-    current_user.ensure_family!
     invite = current_user.invites.new(invite_params)
 
     user = User.find_by(email: invite.email)
-    return render json: { errors: invite.errors.full_messages }, status: :unprocessable_entity if !invite.check_if_invited_by(invite.email) || (user && (user.family == current_user.family || user.family != nil))
+    return render json: { errors: invite.errors.full_messages }, status: :unprocessable_entity if invite.check_if_invited_by(invite.email) || (user && (user.family == current_user.family || user.family != nil))
 
     if invite.save
+      current_user.ensure_family!
       render json: invite, status: :created
     else
       render json: { errors: invite.errors.full_messages }, status: :unprocessable_entity
@@ -41,6 +41,16 @@ class InvitesController < ApplicationController
   def decline
     invite = Invite.pending.find(params[:id])
     invite.decline!
+
+    invited_by = User.find(invite.invited_by.id)
+    family = Family.find(invited_by.family.id)
+
+    if family.users.length <= 1
+      family.destroy!
+      invited_by.family = nil
+      invited_by.save!
+    end
+
     head :no_content
   end
 
